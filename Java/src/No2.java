073d702f79676ebe96831f5b7c7ae3711e024d53
jimin <jjimini98@naver.com>
2021-06-22 00:07:41 +0900

    finished 1000

---
+++ Java/src/No2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class No2 {
+    public static void main(String[]args) {
+
+        //숫자 두 개 입력하기
+        Scanner  number = new Scanner(System.in);
+
+        int num1 = number.nextInt();
+        int num2 = number.nextInt();
+
+        System.out.println(num1 + num2); }
+
+
+}
