65f33345cf9fa50dc285d0342f99a1e7e1f5c56d
jimin <jjimini98@naver.com>
2021-06-22 10:06:34 +0900

    finished 2558

---
+++ Java/src/No3.java
@@ -0,0 +1,17 @@
+//2558. A+B -2
+//실행하면 정상적으로 동작하는데 백준 제출에선 에러
+
+import java.util.Scanner;
+public class No3 {
+    public static void main(String [] args) {
+        Scanner number1 = new Scanner(System.in);
+        Scanner number2 = new Scanner(System.in);
+
+        int num1 = number1.nextInt();
+        int num2 = number2.nextInt();
+
+        System.out.println(num1 + num2); }
+
+    }
+
+
