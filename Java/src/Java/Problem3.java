be6e0c559398a4f3a137c4f7594e352f5dc561a8
jjimini98 <jjimini98@naver.com>
2022-04-04 17:42:20 +0900

    0404 Problem3 완료

---
+++ Java/src/Java/Problem3.java
@@ -0,0 +1,24 @@
+package Java;
+
+// Stdin and Stdout 2
+
+import java.util.Scanner;
+
+public class Problem3 {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String input_int = Integer.toString(scan.nextInt());
+        String input_double = Double.toString(scan.nextDouble());
+//        String input_str = scan.next(); // 공백문자전까지 입력받음
+        scan.nextLine(); // next double에 붙어있던 공백을 받아주는 역할. 그래야 input_str에 값이 들어갈 수 있다.
+        String input_str = scan.nextLine();
+
+
+        System.out.println("String: " + input_str);
+        System.out.println("Double: " + input_double);
+        System.out.println("Int: "+ input_int);
+
+    }
+
+}
