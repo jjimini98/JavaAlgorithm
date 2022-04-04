e4129bee0ce621e5db1cf44ffc4ff04a6866f607
jjimini98 <jjimini98@naver.com>
2022-04-04 19:33:12 +0900

    0404 Problem4 완료 (printf 복습)

---
+++ Java/src/Java/Problem4.java
@@ -0,0 +1,34 @@
+package Java;
+
+import java.util.Scanner;
+
+// printf 정렬에 관한 문제... 복습해두기
+
+public class Problem4 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=scan.next();
+//            int x=scan.nextInt();
+//            scan.nextLine();
+            //Complete this line
+            String x1= Integer.toString(scan.nextInt());
+
+            if (x1.length() < 3) {
+                for (int j = x1.length(); j < 3 ; j++){
+                    x1 = "0"+x1;}
+            }
+            System.out.printf("%-14s %s", s1, x1);
+            System.out.println();
+        }
+        System.out.println("================================");
+
+
+
+    }
+
+
+}
